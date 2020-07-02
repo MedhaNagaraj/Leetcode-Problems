dfcd0fd531ac367f56bdb3b7b4e4e8d86a63c2b4
Medha Nagaraj <54222001+MedhaNagaraj@users.noreply.github.com>
2020-07-02 15:41:34 -0400

    Add files via upload

---
+++ 01 - SQL/02 - 181. Employees Earning More Than Their Managers.sql
@@ -0,0 +1,32 @@
+/*
+
+Solution to: https://leetcode.com/problems/employees-earning-more-than-their-managers/
+The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+
++----+-------+--------+-----------+
+| Id | Name  | Salary | ManagerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | NULL      |
+| 4  | Max   | 90000  | NULL      |
++----+-------+--------+-----------+
+Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.
+
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+*/
+
+-- Write your MySQL query statement below
+
+SELECT 
+    e1.name as "Employee"
+FROM    
+    Employee e1, Employee e2 
+WHERE 
+    e1.salary > e2.salary 
+AND 
+    e1.ManagerId = e2.id
