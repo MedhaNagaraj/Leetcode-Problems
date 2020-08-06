b11b99d3db7a9bd2fd5bdf61c3f3a02a42e9893f
Medha Nagaraj <54222001+MedhaNagaraj@users.noreply.github.com>
2020-08-05 23:33:03 -0400

    Add files via upload

---
+++ 02 - Python/06 - 26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 21:09:55 2020
+
+@author: medha
+"""
+
+nums = [1,1,2]
+print(nums)
+nums = set(nums)
+print(nums)
+nums = list(nums)
+print(nums)
