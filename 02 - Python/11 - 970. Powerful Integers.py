c9fb4f2a1742a61fb7d59c6b0d727bf4307970b7
Medha Nagaraj <54222001+MedhaNagaraj@users.noreply.github.com>
2020-08-23 23:13:09 -0400

    Add files via upload

---
+++ 02 - Python/11 - 970. Powerful Integers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  6 23:54:29 2020
+
+@author: medha
+"""
+"""
+Given two positive integers x and y, an integer is powerful if it is equal to x^i + y^j 
+for some integers i >= 0 and j >= 0.
+
+Return a list of all powerful integers that have value less than or equal to bound.
+
+You may return the answer in any order.  In your answer, each value should occur at most once.
+"""
+x = 2 
+y = 3 
+bound = 10
+#Output: [2,3,4,5,7,9,10]
+
+print(x, y, bound)
+
